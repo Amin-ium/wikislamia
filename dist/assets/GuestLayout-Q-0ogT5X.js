a932a52dae276b1c0d387f28e90dde8dbbff23a3
Amine DAIEF <69642322+Amin-ium@users.noreply.github.com>
2024-06-23 23:56:00 +0100

    commit

--- dist/assets/GuestLayout-Q-0ogT5X.js
+++
@@ -1,8 +0,0 @@
-import { jsx } from "react/jsx-runtime";
-import "@inertiajs/react";
-function Guest({ children }) {
-  return /* @__PURE__ */ jsx("div", { className: "h-screen  flex flex-col   ", children: /* @__PURE__ */ jsx("div", { className: "w-full  h-screen bg-lightBg", children }) });
-}
-export {
-  Guest as G
-};
